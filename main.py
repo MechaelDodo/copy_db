e580f98f32cedc2ca13eb2ce58a485f713fe6629
MechaelDodo <modod@bk.ru>
2022-05-16 00:06:25 +0300

    add dump

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 from mysql.connector import connect, Error
 from getpass import getpass
 from commands_db import *
-
-from standart_connect_args import CONNECT_KWARGS
+import os
+#from standart_connect_args import CONNECT_KWARGS
 from connect_args import *
+
+
+def dump(database, connect_kwargs):
+    os.popen(f"mysqldump -u {connect_kwargs['user']} -p {database} > {database}/{database}_dump.sql")
 
 
 if __name__ == '__main__':
     try:
-        #CONNECT_KWARGS = set_conntect_args()
+        CONNECT_KWARGS = set_conntect_args()
         with connect(**CONNECT_KWARGS) as connection:
             with connection.cursor() as cursor:
                 cursor.execute(SHOW_DATABASES)
@@ -19,5 +23,12 @@ if __name__ == '__main__':
                 select_db = int(input("Select the database which you want to copy:"))
                 select_db = databases[select_db-1]
                 print(select_db)
+                try:
+                    os.mkdir(select_db)
+                    dump(select_db, CONNECT_KWARGS)
+                except FileExistsError:
+                    os.rmdir(select_db)
+                    os.mkdir(select_db)
+                    dump(select_db, CONNECT_KWARGS)
     except Error as e:
-        print(e)
+        print(e, 'haaha')
